82e6d5b0039aa8c82676bcb489eb4016031bf1c3
Stéphane Marcilus <stephane.marcilus@gmail.com>
2020-08-25 15:23:17 +0200

    Exemple d'une création d'une salle de cinéma

---
+++ main/Main.java
@@ -0,0 +1,12 @@
+package main;
+
+import model.SalleCinema;
+
+public class Main {
+    
+    public static void main(String[] args) {
+        
+        SalleCinema mk2_salle1 = new SalleCinema("Retour vers le futur", 7.5, 1000);
+        System.out.println(mk2_salle1);
+    }
+}
